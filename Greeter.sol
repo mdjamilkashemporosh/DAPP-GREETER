e6daefe05c6d0c70fd1820c03253f25a821de501
porosh2002 <jamilkashem2002@gmail.com>
2021-05-19 03:27:42 +0600

    setGreeting and getting Greeting fun() added

--- Greeter.sol
+++ Greeter.sol
@@ -2,5 +2,12 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Greeter{
-    string greeting;
+    string greeting = "Hello";
+    function greet() public view returns( string memory ) {
+        return greeting;
+    }
+    function setGreeting(string memory _greeting) public returns(bool) {
+        greeting = _greeting;
+        return true;
+    }
 }
