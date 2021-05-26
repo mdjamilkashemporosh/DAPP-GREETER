45a2b297cd3cacc023853999681ce864e39b63ad
porosh2002 <jamilkashem2002@gmail.com>
2021-05-26 10:27:03 +0600

    Getting greeting

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,47 @@
+const Greeting = document.getElementById('Greeting')
+const contractAbi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_greeting",
+				"type": "string"
+			}
+		],
+		"name": "setGreeting",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "greet",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+const contractAddress = '0x470ABb20136B511823D202F2d3B8206a25efe31e';
 if (typeof web3 !== 'undefined') {
-    web3 = new Web3(web3.currentProvider);
-  } else {
-    web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
-  }
+  web3 = new Web3(web3.currentProvider);
+} else {
+  web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+}
+
+const contract = new web3.eth.Contract(contractAbi, contractAddress);
+contract.methods.greet().call(function(err, result) {
+  Greeting.innerHTML=result;
+});
+
